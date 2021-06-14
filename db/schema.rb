f01872471d9ab914708575e5ececbecf9a549237
James Yeates <jimibue@gmail.com>
2021-06-14 10:22:20 -0600

    models and seed file setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_163113) do
+ActiveRecord::Schema.define(version: 2021_06_14_155700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "doctor_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "practice"
+    t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "things", force: :cascade do |t|
     t.string "name"
@@ -21,4 +45,6 @@ ActiveRecord::Schema.define(version: 2021_06_02_163113) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "patients"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Thing.create(name:'Thing 1')
-Thing.create(name:'Thing 2')
+Appointment.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+
+d1 = Doctor.create(name:'Dr George', practice:'family', rating:4.5)
+d2 = Doctor.create(name:'Dr Udall', practice:'Heart', rating:3.5)
+d3 = Doctor.create(name:'Dr Z', practice:'brain', rating:4.6)
+
+p1 = Patient.create(name:"Jimbo dubo")
+p2 = Patient.create(name:"John wrigth")
+p3 = Patient.create(name:"Sally Sal")
+
+a1 = Appointment.create(doctor_id: d1.id, patient_id: p1.id, date: DateTime.now + 3)
+Appointment.create(doctor_id: d1.id, patient_id: p1.id, date: DateTime.now + 5)
+Appointment.create(doctor_id: d1.id, patient_id: p1.id, date: DateTime.now + 6)
+Appointment.create(doctor_id: d1.id, patient_id: p2.id, date: DateTime.now + 1)
+Appointment.create(doctor_id: d2.id, patient_id: p1.id, date: DateTime.now + 2)
+Appointment.create(doctor_id: d2.id, patient_id: p3.id, date: DateTime.now + 7)
+
+d1.appointments.create(patient_id: p1.id, date: DateTime.now + 2)
+
+puts d1.patients
+puts a1.doctor
+puts a1.patient
+
